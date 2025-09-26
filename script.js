557c8ea53dcebb1d38a07357862ea9882bd9c6ac
Mabel Teo <mabeltwy@gmail.com>
2025-09-26 21:24:19 +0800

    progress bar

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let time = 25 * 60; // minutes * seconds 
 let mode = "focus"; // keeps track of whether youre in focus/break mode 
 let timer = null; // a variable that stores the ID number returned by setInterval()
+let totalSession = time; // keeps track of how long the current session is supposed to last (in seconds)
 
 // UPDATES THE COUNTDOWN ON SCREEN
 function updateDisplay(){
@@ -12,6 +13,12 @@ function updateDisplay(){
 
 // START OR PAUSE TIMER 
 function startorPauseTimer(){ // function is designed to handle both start and pause
+
+    // RESET
+    if (!timer && time <= 0) { // if timer is not running AND countdown has already reached 0
+        reset();
+    }
+
     // PAUSE
     if (timer){
         clearInterval(timer);
@@ -33,27 +40,49 @@ function setMode(newMode){
     let focusLength = parseInt(focusInput) || 25; // fallback to 25 if input is invalid
     let breakLength = parseInt(breakInput) || 5;   // fallback to 5 if input is invalid
 
-    if (timer){
+    if (timer){ // if timer is running, stop so we can switch modes 
         clearInterval(timer);
         timer = null;
     }
     mode = newMode; 
     time = (mode === "focus" ? focusLength : breakLength) * 60; // focus = 25, break = 5 
-    updateDisplay();
+    totalSession = time; // update new duration 
+
+    reset(); // reset progress bar + countdown display 
     document.getElementById("btnStart").textContent = "START"; // when switching to a new state, reset the button to its original state
 }
 
 // THE HEART OF THE COUNTDOWN -> runs every second because of the setInterval()
 function tick(){
-    time--;
+    if (time > 0){ // prevents negative values 
+        time--;
+    }
     updateDisplay(); 
-    if (time <= 0){ // stop timer if countdown has reached zero 
+
+    // update progress bar to reflect remaining time 
+    let progressUpdate = document.getElementById('progress');
+    progressUpdate.value = totalSession - time; 
+
+    if (time === 0){ // stop timer if countdown has reached zero 
         clearInterval(timer); // stops the repeating setInterval() that was calling tick() every second 
         timer = null; // rests the timer variable to null -> no timer is running now 
         document.getElementById("btnStart").textContent = "START"; // sets button back to "START" so user knows they can start the timer again 
-        let sound = document.getElementById("alarm");
+        
+        // play alert sound 
+        let sound = document.getElementById("alarm"); 
         sound.play();
+
+        reset(); // reset timer + progress bar back to session's full duration
     }
+}
+
+// TIMER/PROGRESS BAR RESET 
+function reset(){
+    let progressUpdate = document.getElementById('progress');
+    time = totalSession; 
+    progressUpdate.max = totalSession; // sets progress bar's max value to match session's total duration 
+    progressUpdate.value = 0; // empty bar so it is ready to fil up in the next session 
+    updateDisplay(); 
 }
 
 // VOLUME VARIABLES 
